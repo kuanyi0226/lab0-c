1863a23eb8943ee6393c1c534ce27373f7becfdf
KuanYi He <kuanyi0226@gmail.com>
2025-03-03 23:33:30 +0800

    Implement q_size
    The q_size function was implemented to traverse the queue
    and count the number of elements.
    It iterates through the list using the list_for_each
    macro from list.h and increments a counter for each element.
    
    If the input head is NULL,
    the function returns -1 to indicate an error.
    
    Change-Id: I942c6f25573674c7a279cfdf29b87ca9970eb3c3

--- queue.c
+++ queue.c
@@ -121,7 +121,16 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return -1;
+
+    int count = 0;
+    struct list_head *current;
+
+    list_for_each (current, head) {
+        count++;
+    }
+    return count
 }
 
 /* Delete the middle node in queue */
